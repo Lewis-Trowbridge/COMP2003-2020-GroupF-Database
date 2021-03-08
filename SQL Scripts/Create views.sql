45f00243a7b5122d0db0848fdd0fb11a299fc50b
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2021-03-08 12:14:12 +0000

    Added app bookings view

--- SQL Scripts/Create views.sql
+++ SQL Scripts/Create views.sql
@@ -21,3 +21,31 @@ LEFT OUTER JOIN venue_tables ON venues.venue_id=venue_tables.venue_id
 
 GROUP BY venues.venue_id, venue_name, venue_postcode, add_line_one, add_line_two, city, county
 GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[app_bookings_view] AS
+
+SELECT 
+bookings.booking_id,
+booking_time,
+booking_size, 
+booking_attended,
+booking_attendees.customer_id,
+venues.venue_id,
+venue_name,
+venue_postcode,
+add_line_one,
+add_line_two,
+city,
+county
+
+FROM bookings, booking_attendees, venues, venue_tables, customers
+
+WHERE bookings.venue_id = venues.venue_id
+AND bookings.venue_table_id = venue_tables.venue_table_id
+AND booking_attendees.booking_id = bookings.booking_id
+AND customers.customer_id = booking_attendees.customer_id
+GO
